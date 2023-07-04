6b96cba53d38db9fefcd266763367d5c6c876aae
EProkofev93 <smileeto@gmail.com.com>
2023-07-04 19:49:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+    Cat barsik = new Cat();
+    Cat bigCat = new Cat(5500, "Murka", 2, "White");
+    Cat smallCat = new Cat(1500, "Kuzya", 1, "Black");
+
+
+
+
+
+    }
+
+}
